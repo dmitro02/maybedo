3803373f0a29af0e30dee09cc23e0804217e1dd0
Dmitry <dmitro02@gmail.com>
2020-07-31 02:22:22 +0300

    utility methids for update slices

--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -48,38 +48,43 @@ export const deleteTaskAction = (task: ITask) => ({
 const tasksReducer = (state: IStore, action: any): IStore => {
   switch (action.type) {
     case "CHANGE_TASK": {
-        const project = state.projects.find((p) => p.id === state.currentProjectId)
-        if (!project) return state
         const { task } = action
-        const i = project.tasks.findIndex(t => t.id === task.id)
-        project.tasks[i] = { ...task }
-        return { ...state }
-    }
-    case "ADD_TASK": {
-        const project = state.projects.find((p) => p.id === state.currentProjectId)
-        if (!project) return state
-        const { task } = action
-        task.id = generateNextId(project.tasks)
-        const newProject = { ...project, tasks: project.tasks.concat(task) }
         return { 
             ...state, 
-            projects: state.projects.map((p) => p !== project ? p : newProject),  
-            justAddedItemId: task.id 
+            projects: updateArray(
+                state.projects, 
+                state.currentProjectId, 
+                (project: IProject) => updateObject(project, 
+                    { tasks: updateArray(
+                        project.tasks, task.id, () => ({ ...task })
+                    )})
+            ) 
         }
     }
-    case "DELETE_TASK": {
-        const project = state.projects.find((p) => p.id === state.currentProjectId)
-        if (!project) return state
+    case "ADD_TASK": {
         const { task } = action
-        project.tasks = project.tasks.filter(t => t !== task)
-        return { ...state }
+        return { 
+            ...state, 
+            projects: updateArray(
+                state.projects, 
+                state.currentProjectId, 
+                (project: IProject) => {
+                    task.id = generateNextId(project.tasks)
+                    return updateObject(project, { tasks: project.tasks.concat(task) }) 
+                }  
+            ),
+            justAddedItemId: task.id
+        }   
+    }
+    case "DELETE_TASK": {
+        return updateProjects(state,
+            (project: IProject) => 
+                updateObject(project, 
+                    { tasks: project.tasks.filter(task => task !== action.task) }))
     }
     case "SET_TITLE": {
-        const project = state.projects.find((p) => p.id === state.currentProjectId)
-        if (!project) return state
-        const { title } = action
-        project.text = title
-        return { ...state }
+        return updateProjects(state,
+                    (project: IProject) => updateObject(project, {text: action.title})) 
     }
     default:
         return state;
@@ -105,8 +110,19 @@ const updateObject = (oldObject: any, newValues: any) => {
     return Object.assign({}, oldObject, newValues)
 }
 
-const updateItemInArray = 
+const updateArray = 
     (array: any[], itemId: number, updateItemCallback: Function) => {
         return array.map((item: any) => 
             item.id !== itemId ? item : updateItemCallback(item))
+}
+
+const updateProjects = (state: IStore, updateProjectCallback: Function) => {
+    return { 
+        ...state, 
+        projects: updateArray(
+            state.projects, 
+            state.currentProjectId, 
+            updateProjectCallback
+        )
+    }
 }
