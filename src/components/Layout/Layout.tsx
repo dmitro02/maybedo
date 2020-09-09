652085cbbe055d9ea5e923166e8280df3fdff000
Dmitry <dmitro02@gmail.com>
2020-09-09 22:13:02 +0300

    show settings instead of tasklist

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Layout.scss'
 import { TasksContextProvider } from '../../contexts/TasksContext'
 import ProjectList from '../ProjectList/ProjectList'
 import TaskList from '../TaskList/TaskList'
+import Divider from '../Divider/Divider'
+import Settings from '../Settings/Settings'
 
 const Layout = () => {
+    const [ isSettingsOpened, setIsSettingsOpened ] = useState(false)
+
+    const toggleSettings = () => {
+        setIsSettingsOpened(!isSettingsOpened)
+    }
 
     return (
         <div className="main-container">
             <TasksContextProvider>
                 <div className="left-panel card">
                     <ProjectList />
+                    <Divider />
+                    <div className="menu">
+                        <div className="menu-item" onClick={toggleSettings}>
+                            <i className="material-icons-outlined">settings</i>
+                            <span>Settings</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="right-panel card">
-                    <TaskList />
+                    {isSettingsOpened 
+                        ? <Settings />
+                        : <TaskList />
+                    }
                 </div>
             </TasksContextProvider>
         </div>
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Settings.scss'
+
+const Settings = () => {
+
+    return (
+        <div>
+            <button>Import</button>
+            <button>Export</button>
+        </div>
+    )
+}
+
+export default Settings
