f36ed4766a811c1974b48733a3ad05bef38924cc
Dmitry <dmitro02@gmail.com>
2020-09-10 22:04:36 +0300

    export done; import in progress

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,14 +1,47 @@
 import React from 'react'
+import { useTasksContext, IStore } from '../../contexts/TasksContext'
 import './Settings.scss'
 
 const Settings = () => {
+    const [ store ] = useTasksContext()
 
     return (
         <div>
-            <button>Import</button>
-            <button>Export</button>
+            <input type="file" id="importData" onChange={handleFileSelect} />
+            <button onClick={() => {}}>Import</button>
+            <button onClick={() => exportData(store)}>Export</button>
         </div>
     )
+}
+
+const excludeKeys = [
+    'path',
+    'selectedSubTaskPath',
+    'addedItemPath'
+]
+
+const replacer = (key: string, value: any) =>
+    excludeKeys.includes(key) ? undefined : value
+
+const exportData = (store: IStore) => {
+    const storedData = JSON.stringify(store, replacer, 2)
+    const dataToExport = 'data:text/json;charset=utf-8,' + storedData
+    const encodedUri = encodeURI(dataToExport)
+    const timestamp = new Date().toISOString()
+    const link = document.createElement('a')
+    link.setAttribute('href', encodedUri)
+    link.setAttribute('download', `todolist_export_${timestamp}.json`);
+    link.click();
+}
+
+const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader()
+    reader.onload = () => {
+       console.log(JSON.parse(reader.result as string));
+    };
+    const files = e.target.files
+    files && reader.readAsText(files[0])
+
 }
 
 export default Settings
