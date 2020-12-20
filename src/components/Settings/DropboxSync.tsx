4c06b966a1837361ceeab02e59d84c34d08f070b
Dmitry <dmitro02@gmail.com>
2020-12-20 21:16:24 +0200

    dropbox authorization

--- src/components/Settings/DropboxSync.tsx
+++ src/components/Settings/DropboxSync.tsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useTasksContext } from '../../contexts/TasksContext'
-import { uploadToDropBox, downloadFromDropBox } from '../../utils/dropBoxUtils'
+import { 
+    uploadToDropBox, 
+    downloadFromDropBox, 
+    generateCodeVerifier,
+    getDropboxAuthUrl,
+    getAndSaveAccessToken
+} from '../../utils/dropBoxUtils'
+
+export const DROPBOX_ACCESS_TOKEN_LS_ITEM_NAME = 'dropboxAccessToken'
 
 const DropboxSync = () => {
     const [ store ] = useTasksContext()
-
     const { rootProject } = store
+
+    const codeVerifier = generateCodeVerifier()
+    const authUrl = getDropboxAuthUrl(codeVerifier)
+
+    const authTokenRef = useRef<HTMLInputElement>(null)
 
     return (
         <div className="settings-block">
             <h2>Dropbox Synchronization</h2>
-            <button onClick={() => uploadToDropBox(rootProject)}>Upload</button>
-            <button onClick={downloadFromDropBox}>Download</button>
+            <p>
+                Get authorization code <a href={authUrl} target="_black">here</a>
+                , insert it into the input below and click "Authorize" button.
+            </p>
+            <input 
+                type="text" 
+                size={43} 
+                placeholder="authorizarion code"
+                ref={authTokenRef}
+            />
+            <button onClick={() => getAndSaveAccessToken(authTokenRef.current?.value, codeVerifier)}>
+                Authorize
+            </button>
+            <div>
+                <button onClick={() => uploadToDropBox(rootProject)}>Upload</button>
+                <button onClick={downloadFromDropBox}>Download</button>
+            </div>
         </div>
     )
 }
--- src/utils/dropBoxUtils.ts
+++ src/utils/dropBoxUtils.ts
@@ -1,13 +1,25 @@
 import { getExportFileNameJson } from './../components/Settings/ExportImport';
 import { Task } from './../types';
-import { TOKEN } from '../localConfig'
 import { Dropbox } from 'dropbox'
 import fetch from 'isomorphic-fetch'
 import { convertDataToJson } from '../components/Settings/ExportImport';
+import { nanoid } from 'nanoid'
 
-const dbx = new Dropbox({ accessToken: TOKEN, fetch })
+const CLIENT_ID = 'lxn28fv9hhsn7id' 
+const AUTH_URL = 'https://www.dropbox.com/oauth2/authorize'
+const GET_TOKEN_URL = 'https://api.dropbox.com/oauth2/token'
+const DROPBOX_ACCESS_TOKEN_LS_ITEM_NAME = 'dropboxAccessToken'
+
+const saveAccessTokenToLS = (accessToken: string) => {
+    localStorage.setItem(DROPBOX_ACCESS_TOKEN_LS_ITEM_NAME, accessToken)
+}
+
+const getAccessTokenFromLS = () => {
+    return localStorage.getItem(DROPBOX_ACCESS_TOKEN_LS_ITEM_NAME) || ''
+}
 
 const getSortedExports = async () => {
+    const dbx = new Dropbox({ accessToken: getAccessTokenFromLS(), fetch })
     const response: any = await dbx.filesListFolder({ path: '' })
     return response.result.entries.sort((a: any, b: any) => {
         const clientModifiedA = new Date(a.client_modified)
@@ -21,6 +33,7 @@ const getSortedExports = async () => {
 }
 
 const getLatestExport = async () => {
+    const dbx = new Dropbox({ accessToken: getAccessTokenFromLS(), fetch })
     const sortedExports = await getSortedExports()
     const path = sortedExports.pop().path_lower
     const response: any = await dbx.filesDownload({ path })
@@ -37,13 +50,50 @@ const readFile = (blob: Blob) => new Promise((resolve, reject) => {
 export const downloadFromDropBox = async () => {
     const latestExport = await getLatestExport()
     const latestExportJson = await readFile(latestExport.fileBlob)
+    console.log(JSON.parse(latestExportJson as string))
     return JSON.parse(latestExportJson as string)
 } 
 
 export const uploadToDropBox = async (root: Task) => {
+    const dbx = new Dropbox({ accessToken: getAccessTokenFromLS(), fetch })
     const contents = convertDataToJson(root)
     const path = '/' + getExportFileNameJson()
     const res = await dbx.filesUpload({ contents, path })
     console.log(res)
 }
 
+export const generateCodeVerifier = () => {
+    return nanoid(43)
+}
+
+export const getDropboxAuthUrl = (codeVerifier: string) => {
+    return `${AUTH_URL}?client_id=${CLIENT_ID}&response_type=code&code_challenge_method=plain&code_challenge=${codeVerifier}`
+}
+
+const getAccessToken = async (authorizationCode: string, codeVerifier: string) => {
+    const body = new URLSearchParams({
+        code: authorizationCode,
+        grant_type: 'authorization_code',
+        client_id: CLIENT_ID,
+        code_verifier: codeVerifier
+    })
+
+    const response = await fetch(GET_TOKEN_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body
+    })
+
+    const resData = await response.json()
+
+    return resData.access_token
+}
+
+export const getAndSaveAccessToken = async (authToken: string|undefined, codeVerifier: string) => {
+    // TODO: display failure/success banners
+    if (!authToken) return
+    const accessToken = await getAccessToken(authToken, codeVerifier)
+    saveAccessTokenToLS(accessToken)
+}
