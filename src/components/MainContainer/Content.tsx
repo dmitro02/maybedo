c0dddd3c256e94da788261d58a1f821e2dd3157f
Dmitry <dmitro02@gmail.com>
2021-02-28 21:38:07 +0200

    right panel component

---
+++ src/components/MainContainer/Content.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useTasksContext } from '../../contexts/TasksContext'
+import Banner from '../Banner/Banner'
+import { 
+    ArrowBackButton, 
+    MenuButton, 
+    SettingsButton 
+} from '../Buttons/Buttons'
+import Fog from '../Fog/Fog'
+import NoProjects from '../NoProjects/NoProjects'
+import TaskList from '../RecordList/TaskList'
+import Settings from '../Settings/Settings'
+import SyncStatus from '../Statuses/SyncStatus'
+import taskStore from '../../utils/Store'
+
+type Props = {
+    isSidebarOpened: boolean,
+    isSettingsOpened: boolean,
+    openSidebar: () => void,
+    toggleSettings: () => void
+}
+
+const Content = (props: Props) => {
+    const { 
+        isSettingsOpened, 
+        isSidebarOpened, 
+        openSidebar,
+        toggleSettings
+    } = props
+
+    const { 
+        store : {
+            loading,
+            syncStatus
+        } 
+    } = useTasksContext()
+
+    const { taskList } = taskStore
+
+    const hasData = !!taskList.tasks.length
+
+    return (
+        <div className="right-panel">
+            <Fog isDisplayed={isSidebarOpened} />
+            <Banner />
+            <div className="top-panel">
+                {!isSettingsOpened &&
+                    <div className="row-btns">
+                        <MenuButton 
+                            action={openSidebar} 
+                            classNames={['open-menu-btn']}
+                            title="open projects list"
+                        />
+                    </div>
+                }
+                <div className="row-btns">
+                    <SyncStatus status={syncStatus} />
+                    {isSettingsOpened 
+                        ? <ArrowBackButton action={toggleSettings} title="close settings" />
+                        : <SettingsButton action={toggleSettings} />
+                    }
+                </div>
+            </div>
+            <div className="right-content">
+                {isSettingsOpened 
+                    ? <Settings backToTaskList={toggleSettings} />
+                    : <TaskList />
+                }
+            </div>
+            {!hasData && !isSettingsOpened && !loading && <NoProjects />} 
+        </div>
+    )
+}
+
+export default Content
--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -1,38 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import './MainContainer.scss'
 import { useTasksContext } from '../../contexts/TasksContext'
-import TaskList from '../RecordList/TaskList'
-import Settings from '../Settings/Settings'
-import Banner from '../Banner/Banner'
-import { 
-    ArrowBackButton, 
-    MenuButton, 
-    SettingsButton 
-} from '../Buttons/Buttons'
-import Fog from '../Fog/Fog'
 import Loading from '../Statuses/Loading'
 import Syncer from '../../utils/Syncer'
-import SyncStatus from '../Statuses/SyncStatus'
-import NoProjects from '../NoProjects/NoProjects'
-import taskStore from '../../utils/Store'
 import Sidebar from '../Sidebar/Sidebar'
+import Content from './Content'
 
 const MainContainer = () => {
     const { 
-        store : {
-            loading,
-            syncStatus
-        }, 
+        store : { loading }, 
         actions 
     } = useTasksContext()
-
-    const { taskList } = taskStore
 
     const [ isSettingsOpened, setIsSettingsOpened ] = useState(false)
 
     const [ isSidebarOpened, setIsSidebarOpened ] = useState(false)
-
-    const hasData = !!taskList.tasks.length
 
     useEffect(() => {
         Syncer.getInstance(actions).initSync()
@@ -60,35 +42,12 @@ const MainContainer = () => {
                 close={closeSidebar} 
                 isSettingsOpened={isSettingsOpened}
             />
-            <div className="right-panel">
-                <Fog isDisplayed={isSidebarOpened} />
-                <Banner />
-                <div className="top-panel">
-                    {!isSettingsOpened &&
-                        <div className="row-btns">
-                            <MenuButton 
-                                action={openSidebar} 
-                                classNames={['open-menu-btn']}
-                                title="open projects list"
-                            />
-                        </div>
-                    }
-                    <div className="row-btns">
-                        <SyncStatus status={syncStatus} />
-                        {isSettingsOpened 
-                            ? <ArrowBackButton action={toggleSettings} title="close settings" />
-                            : <SettingsButton action={toggleSettings} />
-                        }
-                    </div>
-                </div>
-                <div className="right-content">
-                    {isSettingsOpened 
-                        ? <Settings backToTaskList={toggleSettings} />
-                        : <TaskList />
-                    }
-                </div>
-                {!hasData && !isSettingsOpened && !loading && <NoProjects />} 
-            </div>
+            <Content 
+                isSettingsOpened={isSettingsOpened}
+                isSidebarOpened={isSidebarOpened}
+                toggleSettings={toggleSettings}
+                openSidebar={openSidebar}
+            />
         </div>
     )
 }
